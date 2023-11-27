f7ce5bfb2768ccc1d02fb4c7e761df7bdfc00e42
Ayoyinka2456 <108306542+Ayoyinka2456@users.noreply.github.com>
2023-11-27 17:48:28 -0500

    Delete test2.py

--- test2.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import sys
-from flask import Flask
-sys.path.insert(0, './.myenv/bin/python.10')
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return 'Hello World Guys!'
